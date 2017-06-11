6ffbdb8c79ffe8c24d8adc888b01c38f28b72c29
Zoe Lembeck <zlembeck@gmail.com>
2017-06-11 16:54:18 -0500

    created nested routes and actions for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,61 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Projects.all
+  end
+
+  def new
+  end
+  
+  def create
+    project = Project.new(
+                          user_id: params[:user_id],
+                          completed: params[:completed],
+                          current: params[:current],
+                          title: params[:title],
+                          face: params[:face],
+                          face_color: params[:face_color],
+                          eyes: params[:eyes],
+                          nose: params[:nose],
+                          mouth: params[:mouth]
+                          ) 
+    project.save
+
+    redirect_to "/users/#{project.user_id}/projects/#{project.id}" 
+    
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    project.assign_attributes(    
+                              user_id: params[:user_id],
+                              completed: params[:completed],
+                              current: params[:current],
+                              title: params[:title],
+                              face: params[:face],
+                              face_color: params[:face_color],
+                              eyes: params[:eyes],
+                              nose: params[:nose],
+                              mouth: params[:mouth]
+                              )
+
+    project.save
+
+    redirect_to "/users/#{project.user_id}/projects/#{project.id}"  
+  end
+
+  def delete
+    project = Project.find(params[:id])
+    project.destroy
+    
+    redirect_to '/users/#{params[:user_id]}/projects' 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
 Rails.application.routes.draw do
   
+  # users routes 
+  
+
+  get '/users' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/users/:id' => 'users#show'
+
+  get '/users/:id/edit' => 'users#edit'
+  patch '/users/:id' => 'users#update'
+
+  delete '/users:id' => 'users#destroy'
+
+
+  # projects routes
+  get '/projects' => 'projects#index'
+
+  get '/users/:user_id/projects' => 'projects#user_index'
+
+  get '/users/:user_id/projects/new' => 'projects#new'
+  post '/users/:user_id/projects' => 'projects#create'
+
+  get '/users/:user_id/projects/:id' => 'projects#show'
+
+  get '/users/:user_id/projects/:id/edit' => 'projects#edit'
+  patch '/users/:user_id/projects/:id' => 'projects#update'
+
+  delete '/users/:user_id/projects/:id' => 'projects#destroy'
+
+
+
+
  
 end
