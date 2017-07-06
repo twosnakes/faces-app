b43d181ee0ca103d7376d9046d58fb21629b4ae0
Zoe Lembeck <zlembeck@gmail.com>
2017-07-05 21:29:12 -0500

    worked out bugs in bootstrap theme and started adjusting layout

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'jquery-rails', '~> 4.3.1'
+
 gem 'sass-rails', '~> 5.0'
 gem 'seed_dump'
 gem 'bootstrap-sass', '~> 3.3.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
-//= require jquery
 //= require rails-ujs
+//= require jquery 
+//= require bootstrap-sprockets
 //= require vue
 //= require_tree .
